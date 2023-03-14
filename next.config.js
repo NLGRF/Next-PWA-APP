4e77ccd669cc9025561c8b13695cc5baf6869823
Nonpavit Detbun <non.devtech@gmail.com>
2023-03-14 15:06:50 +0700

    demo

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaitings: true,
+    runtimeCaching,
+    disabled: process.env.NODE_ENV === "development",
+    sw: "sw.js",
+  },
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  // i18n: {
+  //   locales: ["pt", "en"],
+  //   defaultLocale: "pt",
+  // },
+  react: {
+    useSuspense: true,
+  },
+  swcMinify: true,
+};
+
+module.exports = [nextConfig, withPWA];
