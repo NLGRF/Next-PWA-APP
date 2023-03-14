8eb3cc9ef0a493304c00e7413bf685aa04acc07e
Nonpavit Detbun <non.devtech@gmail.com>
2023-03-14 15:13:12 +0700

    debig test config

--- next.config.js
+++ next.config.js
@@ -3,11 +3,13 @@ module.exports = {
 };
 
 const withPWA = require("next-pwa");
+const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
   pwa: {
     dest: "public",
     register: true,
     skipWaiting: true,
+    runtimeCaching,
   },
 });
